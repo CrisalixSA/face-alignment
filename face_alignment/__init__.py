30558f95612cda4c0b0a2e1ac8bca8c7610ebf32
Antonio Canela <antonio.canela@crisalix.com>
2023-03-14 16:45:04 +0100

    Added comments

--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -4,4 +4,6 @@ __author__ = """Adrian Bulat"""
 __email__ = 'adrian@adrianbulat.com'
 __version__ = '2.0.1'
 
+# TODO: Add release script (updating it to keep the contents of this file)
+
 from .api import FaceAlignment, LandmarksType, NetworkSize
