fb66d3ec573c8fe5195105a83cc6a428994ab513
Antonio Canela <antonio.canela@crisalix.com>
2023-03-14 16:53:46 +0100

    Changed lines order minor at init file

--- face_alignment/__init__.py
+++ face_alignment/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# TODO: Add release script (updating it to keep the contents of this file)
+
 __author__ = """Adrian Bulat"""
 __email__ = 'adrian@adrianbulat.com'
 __version__ = '2.0.1'
-
-# TODO: Add release script (updating it to keep the contents of this file)
 
 from .api import FaceAlignment, LandmarksType, NetworkSize
